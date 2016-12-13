6046b70729bde1e15c0439fe38c3b00b5c0f55f3
Roman, Jonmer D <jonmer_123@yahoo.com>
2016-12-12 17:07:00 -0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+	int x, y, z, v;
+	
+	printf("Enter value for x: ");
+	scanf("%d", &x);
+	
+	printf("Enter value for y: ");
+	scanf("%d", &y);
+	
+	printf("Enter value for z: ");
+	scanf("%d", &z);
+	
+	
+	printf("%d", v = ((x+y+z)/3));
+}
